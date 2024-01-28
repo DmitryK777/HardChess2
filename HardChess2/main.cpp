818291f9bb25daf0ee5242649877ee2cd0728cdf
DmitryK777 <kislyakovdmv@gmail.com>
2024-01-28 14:03:30 +0300

    Second commit

--- HardChess2/main.cpp
+++ HardChess2/main.cpp
@@ -4,7 +4,15 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
-	int n;
+
+	int n; // Определяет размер как одной клетки так и всей доски
 	cout << "Введите размер доски: "; cin >> n;
 
+	for (int i = 0; i < n * n; i++)
+	{
+		for (int j = 0; j < n * n; j++)
+		{
+
+		}
+	}
 }
